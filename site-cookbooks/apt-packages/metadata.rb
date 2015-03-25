33beaebe5348f3bc078afbcc5e7a6271ae5577f5
Hai Phan <hai.phanthanh@asnet.com.vn>
2015-03-25 13:31:19 +0700

    initial commit

---
+++ site-cookbooks/apt-packages/metadata.rb
@@ -0,0 +1,8 @@
+name             'apt-packages'
+maintainer       'Hai Phan'
+maintainer_email 'hai.phanthanh@asnet.com.vn'
+license          'All rights reserved'
+description      'Installs/Configures apt-packages'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'apt'
